5824e40f0b18eecf604810cae644e5e18f8dadd9
Andy Burke <aburke@bitflood.org>
2016-05-12 16:04:21 -0700

    Allow bursting with new tokenpipe

--- test/03_express.js
+++ test/03_express.js
@@ -161,6 +161,60 @@ test( 'EXPRESS: test route with rate limiting (over limit, 10 seconds)', ( t ) =
     } )();
 } );
 
+test( 'EXPRESS: create burstable route', ( t ) => {
+    const rateController = require( '../index.js' );
+
+    app.get( '/burstable', rateController( {
+        rate: '10/s'
+    } ), ( request, response ) => {
+        response.send( {
+            ok: true
+        } );
+    } );
+
+    t.pass( 'created burstable route' );
+    t.end();
+} );
+
+
+test( 'EXPRESS: test bursting', ( t ) => {
+    const start = Date.now();
+    const time = 1000 * 1;
+    let accepted = 0;
+    let rejected = 0;
+    ( function makeRequest() {
+        const now = Date.now();
+        if ( now - start > time ) {
+            t.ok( accepted, 'got accepted requests' );
+            t.ok( rejected, 'got rejected requests' );
+            t.equal( accepted, 10, '10 accepted requests' );
+            t.end();
+            return;
+        }
+
+        agent
+            .get( `http://localhost:${PORT}/burstable` )
+            .end( ( error, response ) => {
+                if ( !response ) {
+                    t.fail( 'missing response' );
+                    t.end();
+                    return;
+                }
+
+                if ( response.error && response.status !== 429 ) {
+                    t.fail( response.error );
+                    t.end();
+                    return;
+                }
+
+                rejected += response.status === 429 ? 1 : 0;
+                accepted += response.ok ? 1 : 0;
+
+                setTimeout( makeRequest, 10 );
+            } );
+    } )();
+} );
+
 test( 'EXPRESS: stop server', ( t ) => {
     server.close( ( error ) => {
         t.error( error, 'server closed' );
--- test/04_restify.js
+++ test/04_restify.js
@@ -159,6 +159,60 @@ test( 'RESTIFY: test route with rate limiting (over limit, 10 seconds)', ( t ) =
     } )();
 } );
 
+test( 'RESTIFY: create burstable route', ( t ) => {
+    const rateController = require( '../index.js' );
+
+    server.get( '/burstable', rateController( {
+        rate: '10/s'
+    } ), ( request, response ) => {
+        response.send( {
+            ok: true
+        } );
+    } );
+
+    t.pass( 'created burstable route' );
+    t.end();
+} );
+
+
+test( 'RESTIFY: test bursting', ( t ) => {
+    const start = Date.now();
+    const time = 1000 * 1;
+    let accepted = 0;
+    let rejected = 0;
+    ( function makeRequest() {
+        const now = Date.now();
+        if ( now - start > time ) {
+            t.ok( accepted, 'got accepted requests' );
+            t.ok( rejected, 'got rejected requests' );
+            t.equal( accepted, 10, '10 accepted requests' );
+            t.end();
+            return;
+        }
+
+        agent
+            .get( `http://localhost:${PORT}/burstable` )
+            .end( ( error, response ) => {
+                if ( !response ) {
+                    t.fail( 'missing response' );
+                    t.end();
+                    return;
+                }
+
+                if ( response.error && response.status !== 429 ) {
+                    t.fail( response.error );
+                    t.end();
+                    return;
+                }
+
+                rejected += response.status === 429 ? 1 : 0;
+                accepted += response.ok ? 1 : 0;
+
+                setTimeout( makeRequest, 10 );
+            } );
+    } )();
+} );
+
 test( 'RESTIFY: stop server', ( t ) => {
     server.close( ( error ) => {
         t.error( error, 'server closed' );
